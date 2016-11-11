5cca467696661df713da7b6b1fc2c47d2dc1e2c7
Malachi Gray <graymd.cfi@gmail.com>
2016-11-11 16:10:55 -0500

    setting up handle change on edit comment for tech

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,16 +1,35 @@
 import React, { PropTypes } from 'react';
 import './Comment.css';
 
+//currently attempting to edit comment from tech - handleChange
+// receives the change from the input correctly
+function handleChange(e, id) {
+  console.log(e.target.value);
+}
+
 const Comment = ({ item }) => {
+  const customerComment =
+  <span>
+    <h4>{item.author} says...</h4>
+    <p>{item.body}</p>
+  </span>;
+
+  const techComment =
+  <span>
+    <h4>{item.author} says...</h4>
+    <textArea className='editInputText' type='text' name="body" value={item.body} onChange={(e) => handleChange(e, item.id)}/>
+  </span>;
+
   return(
     <li
       className="Comment type"
     >
-      <h4>{item.author} says...</h4>
-      <p>{item.body}</p>
+      {item.type === 'tech' ? techComment : customerComment}
     </li>
   );
 }
+
+
 
 Comment.propTypes = {
   item: PropTypes.shape({
